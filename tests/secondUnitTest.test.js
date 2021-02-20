cc05805797716025427286038ac146f0b08772c4
Jack Volonte <volonte22@up.edu>
2021-02-19 19:55:52 -0800

    final push for hw3
    final push

---
+++ tests/secondUnitTest.test.js
@@ -0,0 +1,12 @@
+var fs = require('fs');
+
+test('test selectEvent', () => {
+	//Read the index.html file into a string
+	var html = fs.readFileSync('public/index.html', 'utf8');
+	expect(html).toEqual(expect.anything()); //any non-nul value is okay
+	
+	//put the HTML into a testing DOM and do a sanity check
+	document.body.innerHTML = html;
+	const $ = require('jquery');
+	expect($('h1').html()).toBe("Cheesecake Order Form");
+});
